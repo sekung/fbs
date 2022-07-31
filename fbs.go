32a91c19f58012ec447cb8b00f3a566e79bb2165
sekung <liusg@rayeye.cn>
2022-07-31 14:19:35 +0800

    v1.1 添加通过事件ID阻塞获取反馈信号

--- fbs.go
+++ fbs.go
@@ -2,11 +2,12 @@ package fbs
 
 import (
 	"errors"
+	"github.com/google/uuid"
 	"sync"
 	"time"
 )
 
-type Single interface {
+type ChannelSingle interface {
 	FeedBack() <-chan backInfo
 	GetSingle() interface{}
 	ToBack(val interface{})
@@ -17,7 +18,7 @@ type backInfo struct {
 	err     error
 }
 
-type single struct {
+type channelSingle struct {
 	source interface{}
 	back   chan backInfo
 	mu     sync.Mutex
@@ -25,11 +26,11 @@ type single struct {
 	in     bool
 }
 
-func (s *single) GetSingle() interface{} {
+func (s *channelSingle) GetSingle() interface{} {
 	return s.source
 }
 
-func (s *single) ToBack(val interface{}) {
+func (s *channelSingle) ToBack(val interface{}) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	if !s.in {
@@ -39,11 +40,11 @@ func (s *single) ToBack(val interface{}) {
 	}
 }
 
-func (s *single) FeedBack() <-chan backInfo {
+func (s *channelSingle) FeedBack() <-chan backInfo {
 	return s.back
 }
 
-func (s *single) timeout() {
+func (s *channelSingle) timeout() {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	if !s.in {
@@ -52,13 +53,73 @@ func (s *single) timeout() {
 	}
 }
 
-func NewSingle(val interface{}, d time.Duration) Single {
+// WithChannel 产生单个信号
+func WithChannel(val interface{}, d time.Duration) ChannelSingle {
 	if d <= 0 {
 		panic(errors.New("must set overtime"))
 	} else {
 		c := make(chan backInfo, 1)
-		s := &single{source: val, back: c}
+		s := &channelSingle{source: val, back: c}
 		s.timer = time.AfterFunc(d, s.timeout)
 		return s
 	}
+}
+
+type EventIdSingle interface {
+	AddSingle(string, time.Duration) <-chan string
+	Done(string, interface{})
+	GetVal(string) (interface{}, bool)
+}
+
+type eventIdSingle struct {
+	backMap map[string]chan string
+	backVal map[string]interface{}
+	mu      sync.Mutex
+}
+
+// AddSingle 向管理器中添加一个信号, 接收信号若是""空，表示timeout。 若不为空，GetVal(反馈值）进行取值
+func (e *eventIdSingle) AddSingle(eventId string, timeout time.Duration) <-chan string {
+	ch := make(chan string, 1)
+	e.mu.Lock()
+	e.backMap[eventId] = ch
+	e.mu.Unlock()
+	time.AfterFunc(timeout, func() {
+		if c, ok := e.backMap[eventId]; ok {
+			e.mu.Lock()
+			c <- ""
+			delete(e.backMap, eventId)
+			e.mu.Unlock()
+		}
+	})
+	return ch
+}
+
+// Done 监测到返回值,添加注入信号管理器
+func (e *eventIdSingle) Done(eventId string, backData interface{}) {
+	e.mu.Lock()
+	defer e.mu.Unlock()
+	if ch, ok := e.backMap[eventId]; ok {
+		id := uuid.NewString()
+		e.backVal[id] = backData
+		ch <- id
+		delete(e.backMap, eventId)
+	}
+}
+
+func (e *eventIdSingle) GetVal(id string) (interface{}, bool) {
+	e.mu.Lock()
+	defer e.mu.Unlock()
+	if val, ok := e.backVal[id]; ok {
+		delete(e.backVal, id)
+		return val, true
+	}
+	return nil, false
+}
+
+func WithEventId() EventIdSingle {
+	return &eventIdSingle{backMap: make(map[string]chan string), backVal: make(map[string]interface{})}
+}
+
+func RandomEventId() string {
+	return uuid.NewString()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/sekung/fbs
 
 go 1.18
+
+require github.com/google/uuid v1.3.0 // indirect
